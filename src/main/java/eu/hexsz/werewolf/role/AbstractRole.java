cdc1f7fae96ab6237d03ba95810b0fc1d065ba96
hexszeug <siemen.zielke@gmail.com>
2022-05-08 20:20:52 +0200

    small PlayerController, AbstractRole change

--- src/main/java/eu/hexsz/werewolf/role/AbstractRole.java
+++ src/main/java/eu/hexsz/werewolf/role/AbstractRole.java
@@ -3,23 +3,37 @@ package eu.hexsz.werewolf.role;
 import eu.hexsz.werewolf.api.IllegalRequestException;
 import eu.hexsz.werewolf.api.Request;
 import eu.hexsz.werewolf.player.Player;
+import lombok.AllArgsConstructor;
 
 /**
  * Contains the handling for the standard player actions. Must be inherited by each role.
  * The class of the {@link PlayerController} stored in the corresponding
  * {@link Player} determines which role the player has.
+ * Also contains the implementation for handling standard requests.
  * @implNote All specific role behavior should only be in the implementation of this class and not in other classes.
  * @since 1.0-SNAPSHOT
  * @author hexszeug
  * */
+@AllArgsConstructor
 public abstract class AbstractRole implements PlayerController {
 
     /**
-     * {@inheritDoc}
+     * Contains the request handling for all non role-specific requests.
+     * Should be called by new implementations when the request is not role-specific.
+     * @param request The request
+     * @throws IllegalRequestException When the request type doesn't exist,
+     * the request can't be handled or
+     * additional data is missing.
      * @since 1.0-SNAPSHOT
      * */
     @Override
     public void handle(Request request) throws IllegalRequestException {
-        PlayerController.super.handle(request);
+        if (request == null) {
+            return;
+        }
+        switch (request.getType()) {
+            //TODO handle day actions
+            default: throw new IllegalRequestException("Request type is not a method.", request);
+        }
     }
 }
--- src/main/java/eu/hexsz/werewolf/role/PlayerController.java
+++ src/main/java/eu/hexsz/werewolf/role/PlayerController.java
@@ -5,27 +5,14 @@ import eu.hexsz.werewolf.api.Request;
 
 /**
  * Is the central control class which handles any player actions.
- * Contains the default request handling for all roles.
+ * The only implementation of this should be {@link AbstractRole}.
  * @since 1.0-SNAPSHOT
  * @author hexszeug
  * */
 public interface PlayerController {
     /**
-     * Contains the request handling for all non role-specific requests.
-     * Should be called by new implementations when the request is not role-specific.
-     * @param request The request
-     * @throws IllegalRequestException When the request type doesn't exist,
-     * the request can't be handled or
-     * additional data is missing.
+     * The only implementation of this should be {@link AbstractRole#handle(Request)}.
      * @since 1.0-SNAPSHOT
      * */
-    default void handle(Request request) throws IllegalRequestException {
-        if (request == null) {
-            return;
-        }
-        switch (request.getType()) {
-            //TODO handle day actions
-            default: throw new IllegalRequestException("Request type is not a method.", request);
-        }
-    }
+    void handle(Request request) throws IllegalRequestException;
 }
